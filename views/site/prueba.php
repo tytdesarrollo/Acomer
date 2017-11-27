4581f6e96af06f90db5178a1b47750e5207213b9
FELLSAT <willysaty1393@hotmail.com>
2017-11-27 11:07:32 -0500

    Mesa, cocina, plaza
    - Tiempos de platos en plaza
    - Funcionamiento de la cocina
    - Registros de clientes en facturacion
    - Seleccion de propina

--- views/site/prueba.php
+++ views/site/prueba.php
@@ -11,11 +11,57 @@
 
 	$request = Yii::$app->request;
 
-	var_dump($result[0]);
+	
+?>
 
+<script type="text/javascript">
 
-	foreach ($result[0] as $key) {
-		echo $key['CONJUNTO'];
+	formatoMoneda('12540230'); 
+
+	function formatoNumerico(valor){
+		var caracter;
+		//valor que se retorna
+		var valorFinal = '';
+
+		for(var i=0 ; i<valor.length ; i++){						
+			
+			caracter = valor.charAt(i);
+
+			if(caracter != "."){
+				valorFinal = valorFinal + caracter;
+			}
+			
+		}		
+
+		console.log(valorFinal);
+		
+		
 	}
 
+	function formatoMoneda(valor){
+		//valor que se retorna
+		var valorFinal1 = '';
+		var valorFinal2 = '';
+
+		//
+		var contadorPos = 1;
+
+		for(var i=valor.length-1 ; i>=0 ; i--){						
+			if(contadorPos == 3 && i != 0){
+				valorFinal1 = valorFinal1 + valor.charAt(i) + '.';
+				contadorPos = 1;
+			}else{
+				valorFinal1 = valorFinal1 + valor.charAt(i);
+				contadorPos++;
+			}			
+		}		
+
+		for(var i=valorFinal1.length ; i>=0 ; i--){
+			valorFinal2 = valorFinal2 + valorFinal1.charAt(i);
+		}
+
+	
+		console.log(valorFinal2); 
+	}
+</script>
 
